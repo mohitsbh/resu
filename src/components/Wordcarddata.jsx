3217b86ab507ce336bb9176b5fcb3f2de95bc6fb
mohitsbh <143165103+mohitsbh@users.noreply.github.com>
2025-01-30 21:54:00 +0530

    Add files via upload

---
+++ src/components/Wordcarddata.jsx
@@ -0,0 +1,213 @@
+import todo from "../assets/todo.jpg";
+import construction from "../assets/construction.jpg";
+import dynamic from "../assets/dynamic-ui.jpg";
+import popup from "../assets/popup.png";
+import web_gallery from "../assets/web-gallery.png";
+import notes from "../assets/notes-ui.png";
+import edusity from "../assets/edusity.png";
+import justice from "../assets/justice-for-sport.png";
+import weatherimg from "../assets/weather-project.png";
+import youtube_img from "../assets/youtube-clone.png";
+import todo_img from "../assets/todolist-project.png"
+import persnolftracker from "../assets/persnol-f-tracker.png"
+import usermanagement from "../assets/usermanagement.png"
+import calc from "../assets/calc.png"
+import weatherweb from "../assets/weather-web.png"
+import resume from "../assets/MohitBhosale.pdf"
+import AudioExtraction from "../assets/audio-extractor.png"
+import paint from "../assets/paint.png"
+import currencycn from "../assets/currencycn.png"
+import snake from "../assets/snake.png"
+import analogclock from "../assets/analogclock.png"
+import solar from "../assets/solar.png"
+import eventcal from "../assets/eventcal.png"
+import hashtag from "../assets/hashtag.png"
+const Wordcarddata = [
+  {
+    id: 1,
+    imgsrc: todo,
+    title: "To Do List",
+    info: "This is To Do List Web App. This is helpful for managing multi tasks in our time.",
+    viewLink: "https://to-do-list-wa.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/todo-app",
+  },
+  {
+    id: 2,
+    imgsrc: construction,
+    title: "Construction UI",
+    info: "This is a user interface of a construction website with projects, services, and contact sections.",
+    viewLink: "https://construction-u.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/construction-ak",
+  },
+  {
+    id: 3,
+    imgsrc: dynamic,
+    title: "Dynamic UI",
+    info: "This is a beautiful dynamic UI website with stunning visuals.",
+    viewLink: "https://meet-u.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/dynamic-ui",
+  },
+  {
+    id: 4,
+    imgsrc: popup,
+    title: "POPUP Message UI",
+    info: "This is poup message UI.",
+    viewLink: "https://pop-up-1.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/popup",
+  },
+  {
+    id: 5,
+    imgsrc: web_gallery,
+    title: "WEB GALLERY",
+    info: " This is beutiful Web Gallery UI website. With some images.",
+    viewLink: "https://web-gallery-1.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/web-gallery",
+  },
+  {
+    id: 6,
+    imgsrc: notes,
+    title: "Notes Web App",
+    info: "This is beautiful Notes web app. Create and delete notes.save in localstorage",
+    viewLink: "https://notes-webapp-1.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/notes-web",
+  },
+  {
+    id: 7,
+    imgsrc: edusity,
+    title: "Edusity clone website",
+    info: " This is Edusity Clone Website. This is made for better experince and also use for colleges for representation.",
+    viewLink: "https://edusity-five-bay.vercel.app/",
+    sourceLink: "https://github.com/mohitsbh/edusity",
+  },
+
+  {
+    id: 8,
+    imgsrc: justice,
+    title: "Justice For Sport",
+    info: "This Website is made for my client.",
+    viewLink: "https://justice-for-sport.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/justice-for-sport",
+  },
+  {
+    id: 9,
+    imgsrc: weatherimg,
+    title: "Weather Project",
+    info: "This Website is made for get a internship.",
+    viewLink: "https://weather-web-ap.netlify.app/dashboard",
+    sourceLink: "https://github.com/mohitsbh/",
+  },
+  {
+    id: 10,
+    imgsrc: todo_img,
+    title: "To-Do-List",
+    info: "This Website is my internship project with Learntricks.",
+    viewLink: "https://todolist-project-2.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/",
+  },
+  {
+    id: 11,
+    imgsrc: youtube_img,
+    title: "Youtube clone",
+    info: " This Website is made for improve skills of React.",
+    viewLink: "https://notes-webapp-1.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/",
+  },
+  {
+    id: 12,
+    imgsrc: persnolftracker,
+    title: "Persnol Finance Tracker",
+    info: " This Website is my internship project.",
+    viewLink: "https://persnol-finance-tracker.vercel.app/",
+    sourceLink: "https://github.com/mohitsbh/persnol-finance-tracker",
+  },
+  {
+    id: 13,
+    imgsrc: usermanagement,
+    title: "User Management System",
+    info: " This Website is my internship project.",
+    viewLink: "https://usermanagement-indol.vercel.app/",
+    sourceLink: "https://github.com/mohitsbh/userManagement/",
+  },
+  {
+    id: 14,
+    imgsrc: calc,
+    title: "Calculator",
+    info: " This Website is my simple javascript lerning project.",
+    viewLink: "https://stately-licorice-b0931a.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/",
+  },
+  {
+    id: 15,
+    imgsrc: weatherweb,
+    title: "Weather",
+    info: "This Website is my simple javascript lerning project with api integration.",
+    viewLink: "https://timely-gelato-e21700.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/",
+  }
+  , {
+    id: 16,
+    imgsrc: AudioExtraction,
+    title: "Audio Extractor",
+    info: " This is My Internship Project.",
+    viewLink: "https://audio-extraction.vercel.app/",
+    sourceLink: "https://github.com/mohitsbh/audio-extraction",
+  }
+  , {
+    id: 17,
+    imgsrc: paint,
+    title: "Gurukrupa Paints",
+    info: " This website created for my client.",
+    viewLink: "https://gurukrupapaints.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/gurukrupa/tree/main/paint",
+  }
+  , {
+    id: 18,
+    imgsrc: currencycn,
+    title: "Currency Converter",
+    info: "This Website is my simple javascript lerning project with api integration.",
+    viewLink: "https://currencycn.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/Currency-converter-",
+  }
+  , {
+    id: 19,
+    imgsrc: snake,
+    title: "Snake And Apple",
+    info: "This Website is my simple javascript lerning project.",
+    viewLink: "https://snake-and-apple.vercel.app/",
+    sourceLink: "https://github.com/mohitsbh/Snake-and-Apple-",
+  }
+  , {
+    id: 20,
+    imgsrc: analogclock,
+    title: "Analog Clock",
+    info: "This Website is my simple javascript lerning project.",
+    viewLink: "https://analog-clock-v1.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/Analog-Clock",
+  }
+  , {
+    id: 21,
+    imgsrc: solar,
+    title: "Solar System",
+    info: "This Website is my simple javascript lerning project.",
+    viewLink: "https://so-l-ar.netlify.app/",
+    sourceLink: "https://github.com/mohitsbh/Solar",
+  }
+  , {
+    id: 22,
+    imgsrc: eventcal,
+    title: "Event Calender",
+    info: "This is my Internship project.",
+    viewLink: "https://eventrrrtf.netlify.a",
+    sourceLink: "https://github.com/mohitsbh/eventcalendar",
+  }
+  , {
+    id: 23,
+    imgsrc: hashtag,
+    title: "Hashtag Generator",
+    info: "This is my Internship project.",
+    viewLink: "https://github.com/user-attachments/assets/30771072-9a3e-4c59-b4cd-74d3af699691",
+    sourceLink: "https://github.com/mohitsbh/hashtag_gen",
+  }
+];
+
+export default Wordcarddata;
